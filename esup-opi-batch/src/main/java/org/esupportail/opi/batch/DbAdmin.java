034805f29b1809e238934cd54df5a8416f94bd6f
Raymond Bourges <raymond.bourges@univ-rennes1.fr>
2012-11-22 11:11:51 +0100

    Ajout du batch DbAdmin (pas totalement fonctionnel)
    Ce batch permet de créer la base de données mais on a encore une
    excaption a cause d'un bean qui est dans la couche vue et donc non
    trouvé. Mais l'exception apparaît après la phase de création de la
    base de données.

---
+++ esup-opi-batch/src/main/java/org/esupportail/opi/batch/DbAdmin.java
@@ -0,0 +1,106 @@
+/**
+ * 
+ */
+package org.esupportail.opi.batch;
+
+import java.util.Properties;
+
+import org.esupportail.commons.services.application.VersionningService;
+import org.esupportail.commons.services.logging.Logger;
+import org.esupportail.commons.services.logging.LoggerImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @author bourges
+ *
+ */
+public class DbAdmin {
+
+	/**
+	 * A logger.
+	 */
+	private static final Logger LOG = new LoggerImpl(DbAdmin.class);
+
+	/**
+	 * Print the syntax and exit.
+	 */
+	private static void syntax() {
+		System.out.print(
+				"syntax: " + DbAdmin.class.getSimpleName() + " <options>\n" +
+				"where option can be:\n" +
+				"- init-db\n" +
+				"- upgrade-db\n");
+	}
+
+	/**
+	 * Dispatch dependaing on the arguments.
+	 * @param args
+	 */
+	protected static void dispatch(final String[] args) {
+		switch (args.length) {
+		case 0:
+			syntax();
+			break;
+		case 1:
+			if ("init-db".equals(args[0])) {
+				LOG.info("Initialise DataBase...");
+				initDb();
+			} else if ("upgrade-db".equals(args[0])) {
+				LOG.info("Upgrade DataBase...");
+				upgradeDb();
+			} else {
+				syntax();
+			}
+			break;
+		default:
+			syntax();
+			break;
+		}
+	}
+
+	/**
+	 * get the versionning service by spring initialization 
+	 * System environment variable generateDdl is set to true in order to force hibernate to generate DDL 
+	 * @return versionning service
+	 */
+	private static VersionningService getVersionningService() {
+		ApplicationContext context =
+			    new ClassPathXmlApplicationContext("properties/applicationContext.xml");
+		VersionningService versionningService =  (VersionningService) context.getBean("versionningService");
+		return versionningService;
+	}
+
+	/**
+	 * Upgrade the database
+	 */
+	private static void upgradeDb() {
+		Properties properties = System.getProperties();
+		properties.put("hbm2ddl", "update");
+		System.setProperties(properties);
+		VersionningService versionningService = getVersionningService();
+		versionningService.upgradeDatabase();
+	}
+
+	/**
+	 * Initialize the database 
+	 */
+	private static void initDb() {
+		Properties properties = System.getProperties();
+		properties.put("hbm2ddl", "create");
+		System.setProperties(properties);
+		VersionningService versionningService = getVersionningService();
+		versionningService.initDatabase();
+	}
+
+	/**
+	 * The main method, called by ant.
+	 * @param args
+	 */
+	public static void main(final String[] args) {
+		dispatch(args);
+	}
+
+
+}
+
