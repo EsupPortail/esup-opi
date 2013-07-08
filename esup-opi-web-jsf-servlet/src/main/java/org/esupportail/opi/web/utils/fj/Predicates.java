127b7c78449589ebdcad0f77311830b146ef809b
haja-opidev <hajaworks@orange.fr>
2013-07-08 10:42:53 +0200

    Resolution #154: apply modularization, add filters into Predicates

--- esup-opi-web-jsf-servlet/src/main/java/org/esupportail/opi/web/utils/fj/Predicates.java
+++ esup-opi-web-jsf-servlet/src/main/java/org/esupportail/opi/web/utils/fj/Predicates.java
@@ -1,5 +1,11 @@
 package org.esupportail.opi.web.utils.fj;
 
+import fj.F;
+import org.esupportail.opi.domain.beans.parameters.TypeTraitement;
+import org.esupportail.opi.domain.beans.user.candidature.Avis;
+import org.esupportail.opi.web.beans.pojo.IndVoeuPojo;
+import org.esupportail.opi.web.beans.pojo.IndividuPojo;
+
 /**
  * Created with IntelliJ IDEA.
  * User: p-opidev
@@ -11,5 +17,49 @@ public class Predicates {
 
     private Predicates() {
         super();
+    }
+
+    /**
+     * Remove items from  a {@link fj.data.Stream} of {@link IndVoeuPojo} matching param
+     *
+     * @param typeTraitement the given param
+     * @return the filtered {@link fj.data.Stream}
+     */
+    public static F<IndVoeuPojo, Boolean> isTraitementNotEquals(final TypeTraitement typeTraitement) {
+        return new F<IndVoeuPojo, Boolean>() {
+            public Boolean f(IndVoeuPojo indVoeuPojo) {
+                return !indVoeuPojo.getTypeTraitement().equals(typeTraitement);
+            }
+        };
+    }
+
+    /**
+     * si onlyValidate = true, on enlève les voeux non validés
+     * et inversement
+     *
+     * @param onlyValidate
+     * @return
+     */
+    public static F<IndVoeuPojo, Boolean> keepOnlyAvisWithValidationEquals(final boolean onlyValidate) {
+        return new F<IndVoeuPojo, Boolean>() {
+            public Boolean f(IndVoeuPojo indVoeuPojo) {
+                Avis avis = indVoeuPojo.getAvisEnService();
+                return avis != null
+                        && avis.getValidation().equals(Boolean.valueOf(onlyValidate));
+            }
+        };
+    }
+
+    /**
+     * Remove items from  a {@link fj.data.Stream} of {@link IndividuPojo} where its {@link IndVoeuPojo} list is empty
+     *
+     * @return the filtered {@link fj.data.Stream}
+     */
+    public static F<IndividuPojo, Boolean> isIndWithoutVoeux() {
+        return new F<IndividuPojo, Boolean>() {
+            public Boolean f(IndividuPojo ip) {
+                return !ip.getIndVoeuxPojo().isEmpty();
+            }
+        };
     }
 }
