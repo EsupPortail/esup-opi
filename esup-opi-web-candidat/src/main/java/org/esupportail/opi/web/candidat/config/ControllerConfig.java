a25801e98ecad71e93701502270900a1af0ee0f2
llevague <lionel.levagueresse@univ-rennes1.fr>
2014-07-04 13:44:06 +0200

    Beginning of auth with spring security

--- esup-opi-web-candidat/src/main/java/org/esupportail/opi/web/candidat/config/ControllerConfig.java
+++ esup-opi-web-candidat/src/main/java/org/esupportail/opi/web/candidat/config/ControllerConfig.java
@@ -7,13 +7,19 @@ import org.esupportail.opi.web.candidat.controllers.CandidaturesController;
 import org.esupportail.opi.web.candidat.controllers.CoordonneesController;
 import org.esupportail.opi.web.candidat.controllers.CursusController;
 import org.springframework.context.annotation.*;
+import org.springframework.security.authentication.AuthenticationServiceException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 import javax.inject.Inject;
 
+import static org.esupportail.opi.web.candidat.services.security.CandidatService.LoggedUser;
 
 
 @Lazy
 @Configuration
+@Import({Security.class})
 public class ControllerConfig {
 
     @Inject
@@ -26,21 +32,31 @@ public class ControllerConfig {
     private I18nService i18nService;
 
     @Bean
+    @Scope(value = "session")
+    public LoggedUser loggedUser() throws AuthenticationException {
+        final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null) {
+            return (LoggedUser) authentication.getPrincipal();
+        }
+        return null;
+    }
+
+    @Bean
     @Scope("view")
     public CoordonneesController coordonneesController() {
-        return CoordonneesController.coordonneesController(domainService, apoService, i18nService);
+        return CoordonneesController.coordonneesController(domainService, apoService, i18nService, loggedUser());
     }
 
     @Bean
     @Scope("view")
     public CursusController cursusController() {
-        return CursusController.cursusController(domainService, apoService, i18nService);
+        return CursusController.cursusController(domainService, apoService, i18nService, loggedUser());
     }
 
     @Bean
     @Scope("view")
     public CandidaturesController candidaturesController() {
-        return CandidaturesController.candidaturesController(domainService, apoService, i18nService);
+        return CandidaturesController.candidaturesController(domainService, apoService, i18nService, loggedUser());
     }
 
 }
--- esup-opi-web-candidat/src/main/java/org/esupportail/opi/web/candidat/controllers/CandidaturesController.java
+++ esup-opi-web-candidat/src/main/java/org/esupportail/opi/web/candidat/controllers/CandidaturesController.java
@@ -3,18 +3,23 @@ package org.esupportail.opi.web.candidat.controllers;
 import org.esupportail.commons.services.i18n.I18nService;
 import org.esupportail.opi.domain.DomainApoService;
 import org.esupportail.opi.domain.DomainService;
+import org.esupportail.opi.web.candidat.services.security.CandidatService;
+
+import static org.esupportail.opi.web.candidat.services.security.CandidatService.LoggedUser;
 
 public class CandidaturesController extends CandidatController {
 
     private CandidaturesController(final DomainService domainService,
                                    final DomainApoService apoService,
-                                   final I18nService i18nService) {
-        super(domainService, apoService, i18nService);
+                                   final I18nService i18nService,
+                                   final LoggedUser loggedUser) {
+        super(domainService, apoService, i18nService, loggedUser);
     }
 
     public static CandidaturesController candidaturesController(final DomainService domainService,
                                                                 final DomainApoService apoService,
-                                                                final I18nService i18nService) {
-        return new CandidaturesController(domainService, apoService, i18nService);
+                                                                final I18nService i18nService,
+                                                                final LoggedUser loggedUser) {
+        return new CandidaturesController(domainService, apoService, i18nService, loggedUser);
     }
 }
